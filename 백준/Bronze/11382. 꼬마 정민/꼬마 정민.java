6002b51a11e4d69855edaa225332993b85600349
김광훈 <35250175+kimgwanghoon@users.noreply.github.com>
2024-07-26 11:19:29 +0900

    [Bronze V] Title: 꼬마 정민, Time: 128 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] a = br.readLine().split(" ");
+        long sum = Arrays.stream(a).mapToLong(Long::parseLong).sum();
+        System.out.println(sum);
+    }
+}
